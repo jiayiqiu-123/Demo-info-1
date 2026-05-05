6ecffcfbd3111ff9719fec0e02143147da07fc73
jiayiqiu-123 <jiayi.qiu@estudiantat.upc.edu>
2026-05-05 09:29:09 +0200

    Mejoras en las primeras funciones

--- LEBL.py
+++ LEBL.py
@@ -3,30 +3,30 @@ import os
 #Definiciones de classes
 
 class Gate:
-    def __init__(self, name):
+    def __init__(self, name, Occupied, id):
         self.Name = name
-        self.Occupied = False
-        self.AircraftID = ""
+        self.Occupied = Occupied
+        self.AircraftID = id
 
 
 class BoardingArea:
-    def __init__(self, name, area_type):
+    def __init__(self, name, area_type, gate):
         self.Name = name
         self.Type = area_type  # "Schengen" o "non-Schengen"
-        self.Gates = []
+        self.Gates = gate
 
 
 class Terminal:
-    def __init__(self, name):
+    def __init__(self, name, BoardA, Airline):
         self.Name = name
-        self.BoardingAreas = []
-        self.Airlines = []  # ICAO codes
+        self.BoardingAreas = BoardA
+        self.Airlines = Airline  # ICAO codes
 
 
 class BarcelonaAP:
-    def __init__(self, code):
+    def __init__(self, code, terminal):
         self.Code = code
-        self.Terminals = []
+        self.Terminals = terminal
 
 
 # Funciones
@@ -41,6 +41,7 @@ def SetGates(area, init_gate, end_gate, prefix):
     while i <= end_gate:
         gate_name = prefix + str(i)
         new_gate = Gate(gate_name)
+        new_gate.Occupied = False
         area.Gates.append(new_gate)
         i = i + 1
 
@@ -48,31 +49,28 @@ def SetGates(area, init_gate, end_gate, prefix):
 
 
 def LoadAirlines(terminal, t_name):
-
     filename = t_name + "_Airlines.txt"
-
-    if not os.path.exists(filename):
-        return -1
-
     try:
         f = open(filename, "r")
-        lines = f.readlines()  # Leemos todas las líneas en una lista
+        temp_list = []
+        line = f.readline()
+
+        while line != "":
+            line = line.strip()
+            if line: # Para asegurar que line haya contenido real (NO espacios)
+                parts = line.split(" ")
+                if len(parts) >= 2:
+                    icao_code = parts[len(parts) - 1]
+                    temp_list.append(icao_code)
+
+            line = f.readline()
+
         f.close()
 
-        new_airlines_list = []
-        i = 0
-        while i < len(lines):
-            line = lines[i].strip()
-            if line:
-                # Separamos por tabulador
-                parts = line.split('\t')
-                if len(parts) >= 2:
-                    icao_code = parts[1].strip()
-                    new_airlines_list.append(icao_code)
-            i = i + 1  # Siguiente línea
-
-        terminal.Airlines = new_airlines_list
+        terminal.Airlines = temp_list
         return 0
 
+    except FileNotFoundError:
+        return -1
     except:
         return -1
